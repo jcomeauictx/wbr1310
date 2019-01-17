5979be080508af87ae3ee668ca6c84a87c54ed3a
John Comeau <jc@unternet.net>
2019-01-16 20:31:15 -0800

    copied tools_time scripts to edit for telnetd

---
+++ html/dlwbr/www/locale/en/dsc/dsc_tools_telnetd.php
@@ -0,0 +1,9 @@
+<h1>Time</h1>
+<strong>Time Configuration</strong><br>
+<br>
+The Time Configuration option allows you to configure, update,
+and maintain the correct time on the internal system clock.
+From this section you can set the time zone that you are in
+and set the NTP (Network Time Protocol) Server.
+Daylight Saving can also be configured to adjust the time when needed
+<br><br>
